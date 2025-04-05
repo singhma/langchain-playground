684f4318ea22e1cf4f63ed7056226345e7fb7bd7
Mannal Singh <mannal.singh@hotmail.com>
2025-04-05 22:12:02 +1100

    Update 5_chatmodel_hf_local.py

--- Models/5_chatmodel_hf_local.py
+++ Models/5_chatmodel_hf_local.py
@@ -1,8 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import os
 
-os.environ['HF_HOME'] = 'D:/huggingface_cache'
-
 llm = HuggingFacePipeline.from_model_id(
     model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
     task='text-generation',
